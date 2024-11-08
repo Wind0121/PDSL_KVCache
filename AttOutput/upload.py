f6b5b700d22bf5efe1fcce1d5e8aa505535d93c8
Wind0121 <1561081334@qq.com>
2024-11-08 10:50:03 +0000

    modify upload.py

--- AttOutput/upload.py
+++ AttOutput/upload.py
@@ -3,3 +3,7 @@ from utils import upload_file_to_oss
 file_path = "/home/zk/PDSL_KVCache/AttOutput/assets/Meta-Llama-3-8B-Instruct/PyramidKV/Llama-3-8B-Instruct_0.png"
 
 print(upload_file_to_oss(file_path))
+
+# https://kv-cache.oss-cn-hangzhou.aliyuncs.com/llama-2-7b-hf_0.png
+# https://kv-cache.oss-cn-hangzhou.aliyuncs.com/longchat-7b-v1.5-32k_0.png
+# https://kv-cache.oss-cn-hangzhou.aliyuncs.com/Llama-3-8B-Instruct_0.png
