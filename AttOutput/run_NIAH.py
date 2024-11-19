c2d78be14b4b17aeafd9c870e4a35ba59b45144e
Wind0121 <1561081334@qq.com>
2024-11-19 12:18:12 +0000

    finish run_NIAH

--- AttOutput/run_NIAH.py
+++ AttOutput/run_NIAH.py
@@ -3,8 +3,31 @@ import torch
 import json
 import numpy as np
 
+def get_same(A, B):
+  count = 0
+  for a in A:
+    if a in B:
+      count += 1
+  return 1.0 * count / len(A)
+
+def cosine_similarity(A, B):
+    # 计算点积
+    dot_product = np.dot(A, B)
+    # 计算向量A和B的范数
+    norm_A = np.linalg.norm(A)
+    norm_B = np.linalg.norm(B)
+    # 计算余弦相似度
+    similarity = dot_product / (norm_A * norm_B)
+    return similarity
+
+def find_first_diff_loc(arr0, arr1):
+  for i, (a, b) in enumerate(zip(arr0, arr1)):
+    if a[0] != b[0]:
+      return 1.0 * i / len(arr0)
+  return 1.0
+
 def evaluate_on_attentions(attentions, query_length):
-  # 三组实验：
+  # 三组对比：
   # 1. prompt0 的 query 在 context 部分的注意力与 context 本身的注意力对比
   # 2. prompt1 的 query 在 context 部分的注意力与 context 本身的注意力对比
   # 3. prompt0 和 prompt1 的 query 在 context 部分的注意力对比
@@ -31,16 +54,59 @@ def evaluate_on_attentions(attentions, query_length):
   query_attentions_1 = [(attention - attention.min()) / (attention.max() - attention.min()) for attention in query_attentions_1]
   assert(query_attentions_0[0].shape[0] == query_attentions_1[0].shape[0])
 
+  # test1
+  sort_context_attentions_0 = [[(i, score) for i, score in enumerate(attention)] for attention in context_attentions_0]
+  sort_context_attentions_0 = [sorted(layer_scores, key=lambda x: x[1], reverse=True) for layer_scores in sort_context_attentions_0]
+  sort_context_attentions_1 = [[(i, score) for i, score in enumerate(attention)] for attention in context_attentions_1]
+  sort_context_attentions_1 = [sorted(layer_scores, key=lambda x: x[1], reverse=True) for layer_scores in sort_context_attentions_1]
+
+  sort_query_attentions_0 = [[(i, score) for i, score in enumerate(attention)] for attention in query_attentions_0]
+  sort_query_attentions_0 = [sorted(layer_scores, key=lambda x: x[1], reverse=True) for layer_scores in sort_query_attentions_0]
+  sort_query_attentions_1 = [[(i, score) for i, score in enumerate(attention)] for attention in query_attentions_1]
+  sort_query_attentions_1 = [sorted(layer_scores, key=lambda x: x[1], reverse=True) for layer_scores in sort_query_attentions_1]
+
+  first_diff_loc_context0_query0 = []
+  first_diff_loc_context1_query1 = []
+  first_diff_loc_query0_query1 = []
+  for arr0, arr1 in zip(sort_context_attentions_0, sort_query_attentions_0):
+    first_diff_loc_context0_query0.append(find_first_diff_loc(arr0, arr1))
+  for arr0, arr1 in zip(sort_context_attentions_1, sort_query_attentions_1):
+    first_diff_loc_context1_query1.append(find_first_diff_loc(arr0, arr1))
+  for arr0, arr1 in zip(sort_query_attentions_0, sort_query_attentions_1):
+    first_diff_loc_query0_query1.append(find_first_diff_loc(arr0, arr1))
+
   # test2
-  l2_dis_0 = [np.linalg.norm(context - query) for context, query in zip(context_attentions_0, query_attentions_0)]
-  l2_dis_1 = [np.linalg.norm(context - query) for context, query in zip(context_attentions_1, query_attentions_1)]
-  l2_dis_2 = [np.linalg.norm(query_0 - query_1) for query_0, query_1 in zip(query_attentions_0, query_attentions_1)]
-  return [l2_dis_0, l2_dis_1, l2_dis_2]
+  l2_context0_query0 = [float(np.linalg.norm(context - query)) for context, query in zip(context_attentions_0, query_attentions_0)]
+  l2_context1_query1 = [float(np.linalg.norm(context - query)) for context, query in zip(context_attentions_1, query_attentions_1)]
+  l2_query0_query1 = [float(np.linalg.norm(query_0 - query_1)) for query_0, query_1 in zip(query_attentions_0, query_attentions_1)]
+
+  # test3
+  cos_context0_query0 = [float(cosine_similarity(context, query)) for context, query in zip(context_attentions_0, query_attentions_0)]
+  cos_context1_query1 = [float(cosine_similarity(context, query)) for context, query in zip(context_attentions_1, query_attentions_1)]
+  cos_query0_query1 = [float(cosine_similarity(query_0, query_1)) for query_0, query_1 in zip(query_attentions_0, query_attentions_1)]
+
+  # test4
+  top_context_attention_0 = [attention[:int(0.15 * len(attention))] for attention in sort_context_attentions_0]
+  top_context_attention_0 = [[pt[0] for pt in attention] for attention in top_context_attention_0]
+  top_context_attention_1 = [attention[:int(0.15 * len(attention))] for attention in sort_context_attentions_1]
+  top_context_attention_1 = [[pt[0] for pt in attention] for attention in top_context_attention_1]
+  
+  top_query_attention_0 = [attention[:int(0.15 * len(attention))] for attention in sort_query_attentions_0]
+  top_query_attention_0 = [[pt[0] for pt in attention] for attention in top_query_attention_0]
+  top_query_attention_1 = [attention[:int(0.15 * len(attention))] for attention in sort_query_attentions_1]
+  top_query_attention_1 = [[pt[0] for pt in attention] for attention in top_query_attention_1]
+
+  inter_context0_query0 = [len(set(a) & set(b)) for a, b in zip(top_context_attention_0, top_query_attention_0)]
+  inter_context1_query1 = [len(set(a) & set(b)) for a, b in zip(top_context_attention_1, top_query_attention_1)]
+  inter_query0_query1 = [len(set(a) & set(b)) for a, b in zip(top_query_attention_0, top_query_attention_1)]
+
+  return (first_diff_loc_context0_query0, first_diff_loc_context1_query1, first_diff_loc_query0_query1), (l2_context0_query0, l2_context1_query1, l2_query0_query1), \
+          (cos_context0_query0, cos_context1_query1,cos_query0_query1), (inter_context0_query0, inter_context1_query1, inter_query0_query1)
 
 
 def main():
-  model_path = '/data/llm/Meta-Llama-3-8B-Instruct'
-  data_path = 'data/NIAH.txt'
+  model_path = '/data/llm/longchat-7b-v1.5-32k'
+  data_path = 'data/mini_NIAH.txt'
   needles = ('The best thing to do in San Francisco is eat a sandwich and sit in Dolores Park on a sunny day.', 'The weather in New York is cold.')
   retrieval_query = ('The best thing to do in San Francisco is: ', 'The weather in New York is: ')
   save_path = 'NIAH_result.json'
@@ -54,20 +120,20 @@ def main():
 
   model_name = model_path.split('/')[-1]
   model = AutoModelForCausalLM.from_pretrained(
-                    model_name, 
+                    model_path, 
                     torch_dtype=torch.float16,
-                    attn_implementation='flash_attention_2',
+                    # attn_implementation='flash_attention_2',
                     device_map="auto",
                     low_cpu_mem_usage=True, 
                     use_cache=True, 
                     trust_remote_code=True
                     ).eval()
-  tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+  tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
   prompts = [tokenizer(prompt, return_tensors="pt") for prompt in prompts]
   input_ids = [prompt['input_ids'].to(model.device) for prompt in prompts]
 
-  output_ids = [model(
+  output_ids = [model.generate(
                   input_id, 
                   output_attentions=False,
                   max_new_tokens=30,
@@ -80,24 +146,28 @@ def main():
               ]
   responses = [tokenizer.decode(output_id[0][input_id.shape[1]:], skip_special_tokens=True).strip() for output_id, input_id in zip(output_ids, input_ids)]
 
-  querys = [tokenizer(query, return_tensor="pt") for query in querys]
+  querys = [tokenizer(query, return_tensors="pt") for query in querys]
   query_ids = [query['input_ids'].to(model.device) for query in querys]
   query_length = [query_id.shape[1] for query_id in query_ids]
-  attentions =  [model(
-                      input_id, 
-                      output_attentions=True
-                  ).attentions
-                  for input_id in input_ids
-                ]
-  l2_dis = evaluate_on_attentions(attentions, query_length)
+  attentions = [model(input_id, output_attentions=True).attentions for input_id in input_ids]
+  first_diff, l2, cos, inter = evaluate_on_attentions(attentions, query_length)
 
   result = {
     'model' : model_name,
     'needle' : needles,
     'model_response' : responses,
-    'l2_dis_0' : l2_dis[0],
-    'l2_dis_1' : l2_dis[1],
-    'l2_dis_2' : l2_dis[2]
+    'first_diff_loc_context0_query0' : first_diff[0],
+    'first_diff_loc_context1_query1' : first_diff[1],
+    'first_diff_loc_query0_query1' : first_diff[2],
+    'l2_context0_query0' : l2[0],
+    'l2_context1_query1' : l2[1],
+    'l2_query0_query1' : l2[2],
+    'cos_context0_query0' : cos[0],
+    'cos_context1_query1' : cos[1],
+    'cos_query0_query1' : cos[2],
+    'inter_context0_query0' : inter[0],
+    'inter_context1_query1' : inter[1],
+    'inter_query0_query1' : inter[2]
   }
 
   with open(save_path, 'w') as f:
