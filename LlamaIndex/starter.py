d49eeb622cd1f24796ed7ad9cc942635a74bfd3e
Wind0121 <1561081334@qq.com>
2024-11-25 06:57:08 +0000

    can run LlamaIndex now

--- LlamaIndex/starter.py
+++ LlamaIndex/starter.py
@@ -4,21 +4,12 @@ import sys
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
+import torch
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_index.llms.huggingface import HuggingFaceLLM
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import Settings
-import torch
-
-# setup prompts - specific to StableLM
 from llama_index.core import PromptTemplate
-
-system_prompt = """<|SYSTEM|># StableLM Tuned (Alpha version)
-- StableLM is a helpful and harmless open-source AI language model developed by StabilityAI.
-- StableLM is excited to be able to help the user, but will refuse to do anything that could be considered harmful to the user.
-- StableLM is more than just an information source, StableLM is also able to write poetry, short stories, and make jokes.
-- StableLM will refuse to participate in anything that could harm a human.
-"""
 
 # This will wrap the default prompts that are internal to llama-index
 query_wrapper_prompt = PromptTemplate("<|USER|>{query_str}<|ASSISTANT|>")
@@ -26,28 +17,22 @@ query_wrapper_prompt = PromptTemplate("<|USER|>{query_str}<|ASSISTANT|>")
 # bge-base embedding model
 Settings.embed_model = HuggingFaceEmbedding(model_name="/data/llm/bge-small-en-v1.5")
 
-llm = HuggingFaceLLM(
-    context_window=4096,
-    max_new_tokens=256,
-    generate_kwargs={"temperature": 0.7, "do_sample": False},
-    system_prompt=system_prompt,
+# select hf model
+# models already run: [longchat-7b-v1.5-32k, Qwen2.5-7B-Instruct, stablelm-tuned-alpha-3b$]
+# you can select other parameter
+Settings.llm = HuggingFaceLLM(
     query_wrapper_prompt=query_wrapper_prompt,
-    tokenizer_name="/data/llm/stablelm-tuned-alpha-3b",
-    model_name="/data/llm/stablelm-tuned-alpha-3b",
+    tokenizer_name="/data/llm/longchat-7b-v1.5-32k",
+    model_name="/data/llm/longchat-7b-v1.5-32k",
     device_map="auto",
-    stopping_ids=[50278, 50279, 50277, 1, 0],
-    tokenizer_kwargs={"max_length": 4096},
-    # uncomment this if using CUDA to reduce memory usage
-    # model_kwargs={"torch_dtype": torch.float16}
 )
 
-Settings.llm = llm
-Settings.chunk_size = 1024
-
-# load documents
+# load data
 documents = SimpleDirectoryReader("data").load_data()
+# build index
 index = VectorStoreIndex.from_documents(documents)
-# set Logging to DEBUG for more detailed outputs
+# build query_engine which use llm above
 query_engine = index.as_query_engine()
+# chat
 response = query_engine.query("What did the author do growing up?")
 print(response)
